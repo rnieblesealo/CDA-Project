52d5ed76b5b470757e0a0c50ad702599f8b4125b
Luke Sandoval <118379340+lukesan48@users.noreply.github.com>
2023-11-28 17:01:57 -0500

    finished instruction_decode function

--- project.c
+++ project.c
@@ -90,7 +90,103 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-    
+    // initialize all control signals to 0
+    controls->ALUOp = 0;
+    controls->ALUSrc = 0;
+    controls->Branch = 0;
+    controls->Jump = 0;
+    controls->MemRead = 0;
+    controls->MemtoReg = 0;
+    controls->MemWrite = 0;
+    controls->RegDst = 0;
+    controls->RegWrite = 0;
+
+    switch(op) {
+        // 0000 0000 r-types (add, sub, &, or, slt, sltu)
+        case 0: 
+          controls->RegDst = 1;
+          controls->RegWrite = 1;
+          
+          // for all r-types
+          controls->ALUOp = 7;
+          break;
+        
+        // 0010 0011 lw
+        case 35:
+          controls->ALUSrc = 1;
+          controls-> MemtoReg = 1;
+          controls->RegWrite = 1;
+          controls->MemRead = 1;
+          break;
+
+        // 0010 1011 sw
+        case 43:
+          controls->RegDst = 2;
+          controls->ALUSrc = 1;
+          controls->MemtoReg = 2;
+          controls->MemWrite = 1;
+          break;
+
+        // 0000 1000 addi
+        case 8:
+          controls->ALUSrc = 1;
+          controls->RegWrite = 1;
+          
+          // add ALUopcode
+          controls->ALUOp = 0;
+          break;
+
+        // 0000 1111 lui
+        case 15:
+
+        // 0000 0100 beq
+        case 4:
+          controls->RegDst = 2;
+          controls->MemtoReg = 2;
+          controls->Branch = 1;
+          
+          // sub ALUopcode
+          controls->ALUOp = 1;
+          break;
+
+        //0000 1010 slti
+        case 10:
+          controls->RegWrite = 1;
+          controls->ALUSrc = 1;
+          
+          // slt ALUopcode
+          controls->ALUOp = 2;
+          break;
+
+        // 0000 1011 sltiu
+        case 11:
+          controls->RegWrite = 1;
+          controls->ALUSrc = 1;
+          
+          // sltu ALUopcode
+          controls->ALUOp = 3;
+          break;
+
+        // 0000 0010 j
+        case 2:
+          controls->Jump = 1;
+          controls->Branch = 2;
+          controls->MemtoReg = 2;
+          controls->ALUSrc = 2;
+          controls->RegDst = 2; 
+
+          // slt ALUopcode
+          controls->ALUOp = 2;
+          break;
+
+        default:
+          // case to assert halt
+          return 1;
+    }
+
+    // else return 0 and deassert Halt
+    return 0;
+
 }
 
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
